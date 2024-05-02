81b45a545ccec556bcb164d55f24da761977b677
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2024-05-02 13:44:09 +0200

    Add mariadb service

---
+++ files/srcs/requirements/mariadb/conf/mysql.sh
@@ -0,0 +1,25 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    mysql.sh                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/05/15 14:54:39 by acrespy           #+#    #+#              #
+#    Updated: 2023/05/16 15:01:53 by acrespy          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+
+service mariadb start;
+
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
+
+exec mysqld_safe
+
