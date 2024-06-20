2031a84ec03daacf7cc1ed31ace1cd0bda6bd2d6
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2024-06-20 18:33:30 +0200

    Update

--- files/srcs/requirements/mariadb/conf/mysql.sh
+++ files/srcs/requirements/mariadb/conf/mysql.sh
@@ -6,10 +6,9 @@ sleep 10
 
 mysql -uroot -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
 
-mysql -uroot -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%.srcs_inception' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%.srcs_inception' WITH GRANT OPTION;"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'wordpress.srcs_inception' WITH GRANT OPTION;"
-mysql -uroot -e "ALTER USER '${MYSQL_USER}'@'%.srcs_inception' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -uroot -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@''%''.srcs_inception' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@''%''.srcs_inception' WITH GRANT OPTION;"
+mysql -uroot -e "ALTER USER '${MYSQL_USER}'@''%''.srcs_inception' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
 mysql -uroot -e "CREATE USER IF NOT EXISTS '${MYSQL_ROOT_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ROOT_USER}'@'%' WITH GRANT OPTION;"
--- files/srcs/requirements/wordpress/conf/wordpress.sh
+++ files/srcs/requirements/wordpress/conf/wordpress.sh
@@ -1,22 +1,19 @@
 #!/bin/sh
 
-if [ ! -d /var/www/wordpress ]
-then
-  mkdir -p /var/www/wordpress
-  wp core download --allow-root --path='/var/www/wordpress'
-fi
+wp core download --allow-root --path='/var/www/wordpress'
 
 if [ ! -e /var/www/wordpress/wp-config.php ]
 then
-  wp config create --allow-root --dbname="${MYSQL_DATABASE}" --dbuser="${MYSQL_USER}" --dbpass="${MYSQL_PASSWORD}" --dbhost=mariadb:3306 --path='/var/www/wordpress'
-  sleep 10
-  wp core install --allow-root --url="${DOMAIN_NAME}" --title="${WP_TITLE}" --admin_user="${WP_ADMIN_USER}" --admin_password="${WP_ADMIN_PASSWORD}" --admin_email="${WP_ADMIN_EMAIL}" --path='/var/www/wordpress'
-  wp user create --allow-root --role=author "${WP_USER_USER}" "${WP_USER_EMAIL}" --user_pass="${WP_USER_PASS}" --path='/var/www/wordpress' >> /log.txt
-fi
+  # https://github.com/wp-cli/wp-cli/issues/5933 (Could not create 'wp-config.php' with wp-cli config create #5933)
+  cd /var/www/wordpress || return
 
-if [ ! -d /run/php ]
-then
-    mkdir -p /run/php
+  wp config create --dbname="${MYSQL_DATABASE}" --dbuser="${MYSQL_USER}" --dbpass="${MYSQL_PASSWORD}" --dbhost=mariadb:3306 --allow-root
+  sleep 5
+
+  wp core install --url="${DOMAIN_NAME}" --title="${WP_TITLE}" --admin_user="${WP_ADMIN_USER}" --admin_password="${WP_ADMIN_PASSWORD}" --admin_email="${WP_ADMIN_EMAIL}" --allow-root
+  sleep 5
+
+  wp user create --role=author "${WP_USER_USER}" "${WP_USER_EMAIL}" --user_pass="${WP_USER_PASS}" --allow-root >> /log.txt
 fi
 
 php-fpm8.2 -F
